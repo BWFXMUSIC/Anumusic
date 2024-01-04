16a57191873745a34e2bb413e1fb3fb6887596ba
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-01-04 09:20:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 6950353753))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Anufromdelhimusic_bot")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BlackMusicSupport")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Exampurrrrrr")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
